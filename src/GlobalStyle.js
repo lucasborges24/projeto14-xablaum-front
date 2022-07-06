e431e284ee2f90ccbecdd2171d2b3e1d552af161
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-06 12:33:42 -0300

    feat: add Header mobile layout

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -9,6 +9,7 @@ ${reset}
 
     body {
         background-color: #011017;
+        background-image: url('https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?cs=srgb&dl=pexels-adrien-olichon-2387793.jpg&fm=jpg');
         font-family: 'Poppins', sans-serif;
     }
 `
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -49,6 +49,7 @@ const HeaderStyle = styled.header`
     left: 0; top: 0;
     width: 100%;
     height: 112px;
+    box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
     background-color: #0060b1;
 `
 
@@ -61,6 +62,10 @@ const Head = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    @media (max-width:  935px) {
+        padding: 0 10px;
+    }
 `
 
 const H1 = styled.h1` 
@@ -77,6 +82,10 @@ const H1 = styled.h1`
         color: #0a437a;
     }
 
+    @media (max-width:  935px) {
+        font-size: 28px;
+        margin-right: 15px;
+    }
 `
 
 const HeadRight = styled.div` 
@@ -123,6 +132,10 @@ const IconCart = styled(Link)`
     ion-icon {
         font-size: 27px;
         color: #c4c4c4;
+    }
+
+    @media (max-width:  935px) {
+        margin-right: 15px;
     }
 `
 
