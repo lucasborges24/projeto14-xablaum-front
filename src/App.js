abffcd7ccf6d6fd3cb8a3870b4f6714391e4c4ed
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-08 17:52:54 -0300

    feat: add userContext && create URL req

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-import Login from './pages/Login';
-import Cadastro from './pages/Cadastro';
-import Header from './shared/Header';
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Header from "./shared/Header";
+import UserContext from "./contexts/UserContext";
 
 function App() {
-
-  const [userToken, setUserToken] = useState({})
+  const [userToken, setUserToken] = useState({});
+  const URL = "https://xablaum.herokuapp.com";
 
   return (
-    <BrowserRouter>
-      <Header userToken={userToken}/>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/cadastro' element={<Cadastro />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider
+      value={{
+        URL,
+      }}
+    >
+      <BrowserRouter>
+        <Header userToken={userToken} />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
