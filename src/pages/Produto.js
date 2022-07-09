1d046a760fa5028cbdcf05724d8197d329f57a67
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-09 11:28:32 -0300

    feat: finish product page layout

--- src/pages/Produto.js
+++ src/pages/Produto.js
@@ -6,7 +6,7 @@ function Produto() {
     oldPrice: 888.78,
     newPrice: 699.9,
     image:
-      "https://images.kabum.com.br/produtos/fotos/328743/monitor-brazilpc-21-5-led-full-hd-75hz-hdmi-vga-vesa-preto-22w-75kan_1650400899_gg.jpg",
+      "https://images.kabum.com.br/produtos/fotos/328743/monitor-brazilpc-21-5-led-full-hd-75hz-hdmi-vga-vesa-preto-22w-75kan_1650400898_gg.jpg",
     description: [
       {
         title: "VESA 100x100",
@@ -33,37 +33,228 @@ function Produto() {
 
   return (
     <Body>
-        <Main></Main>
-        <Description></Description>
+      <Main>
+        <H2>{product.name}</H2>
+        <MainBody>
+          <Img>
+            <img src={product.image} alt="produto" />
+          </Img>
+          <Content>
+            <Price>
+              <h4>{product.oldPrice}</h4>
+              <h3>R${product.newPrice.toFixed(2).replace(".", ",")}</h3>
+            </Price>
+            <AddCart>
+              <ion-icon name="cart"></ion-icon>
+              <p>Comprar</p>
+            </AddCart>
+          </Content>
+        </MainBody>
+      </Main>
+      <Description>
+        <DescriptionTitle>
+          <ion-icon name="document"></ion-icon>
+          <H2>Descrição do produto</H2>
+        </DescriptionTitle>
+        <H2>{product.name}</H2>
+        <DescriptionSubTitle>
+          {product.description.map((i) => {
+            return (
+              <>
+                <h5>{i.title}</h5>
+                <h6>{i.text}</h6>
+              </>
+            );
+          })}
+        </DescriptionSubTitle>
+      </Description>
     </Body>
   );
 }
 
-const Main = styled.main` 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #ffffff;
-    margin: 10px 15px;
-    height: 20px;
-`
-
 const Body = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #f2f3f4;
-    padding-top: 40px;
-`
+  width: 100%;
+  height: 100%;
+  background-color: #f2f3f4;
+  padding: 40px 0;
+`;
 
-const Description = styled.div` 
-    display: flex;
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  background-color: #ffffff;
+  margin: 10px 15px;
+  padding: 2rem;
+  border-radius: 0.25rem;
+  /* width: 100%; */
+`;
+
+const H2 = styled.h2`
+  font-size: 1.5rem;
+  line-height: 2.125rem;
+  font-weight: 700;
+  color: rgb(66, 70, 77);
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+`;
+
+const MainBody = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+
+  @media (max-width: 715px) {
     flex-direction: column;
-    align-items: center;
+  }
+`;
+
+const Img = styled.div`
+  width: 50%;
+  height: 50%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media (max-width: 715px) {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 50%;
+  height: 100%;
+  margin-bottom: 3.5rem;
+
+  @media (max-width: 715px) {
     justify-content: space-between;
-    background-color: #ffffff;
-    margin: 10px 15px;
-    height: 20px;
-`
+    width: 100%;
+  }
+
+  @media (max-width: 420px) {
+    flex-direction: column;
+
+    h3 {
+        margin-bottom: 15px;
+    }
+  }
+`;
+
+const Price = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  margin-right: 10px;
+
+  h4 {
+    font-size: 0.875rem;
+    line-height: 1.3125rem;
+    color: rgb(127, 133, 141);
+    text-decoration: line-through;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+    line-height: 3rem;
+    font-weight: 700;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    color: rgb(255, 101, 0);
+  }
+
+    @media (max-width: 420px) {
+        align-items: center;
+
+        h4 {
+            text-align: left;
+        }
+    }
+ 
+`;
+
+const AddCart = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(255, 101, 0);
+  max-width: 248px;
+  width: 100%;
+  height: 48px;
+  margin: 0 auto;
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 18px;
+  padding: 13px;
+  border: 1px solid rgb(255, 101, 0);
+  border-radius: 0.25rem;
+  cursor: pointer;
+
+  @media (max-width: 420px) {
+
+  }
+`;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  background-color: #ffffff;
+  margin: 40px 15px;
+  padding: 2rem;
+  border-radius: 0.25rem;
+
+  h2 {
+    font-size: 1rem;
+    padding: 0;
+    margin: 32px 0 0 3px;
+  }
+`;
+
+const DescriptionTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  ion-icon {
+    color: rgb(255, 101, 0);
+    font-size: 30px;
+  }
+
+  h2 {
+    padding: 0;
+    margin: 0 0 0 15px;
+    font-size: 1.5rem;
+  }
+`;
+
+const DescriptionSubTitle = styled.div`
+
+    padding-left: 3px;
+    color: rgb(86, 92, 105);
+
+    h5 {
+        font-size: 16px;
+        font-weight: bold;
+        margin: 10px 0;
+    }
+
+    h6 {
+        margin-bottom: 25px;
+    }
+`;
 
 export default Produto;
