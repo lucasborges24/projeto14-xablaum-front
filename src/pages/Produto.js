96b490f192d735aa2c7c41c87840483b991df095
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-09 09:39:35 -0300

    feat: mock an product object && create main divs with styled components

--- src/pages/Produto.js
+++ src/pages/Produto.js
@@ -1,11 +1,50 @@
+import styled from "styled-components";
 
+function Produto() {
+  const product = {
+    name: "Monitor Brazilpc 21.5 LED Full HD, 75Hz, HDMI/VGA, VESA, Preto - 22W-75KAN",
+    oldPrice: 888.78,
+    newPrice: 699.9,
+    image:
+      "https://images.kabum.com.br/produtos/fotos/328743/monitor-brazilpc-21-5-led-full-hd-75hz-hdmi-vga-vesa-preto-22w-75kan_1650400899_gg.jpg",
+    description: [
+      {
+        title: "VESA 100x100",
+        text: "O Monitor conta com VESA que é um padrão de quatro furos localizado na parte traseira das TVs e Monitores. Esse padrão VESA determina a compatibilidade entre a TV e o suporte de fixação.",
+      },
+      {
+        title: "Resolução Full HD",
+        text: "Full HD, também conhecido como 1080p ou FHD (alta definição total), é uma resolução de tela muito comum de 1920 x 1080 pixels. Esse monitor passa exibe uma imagem de 1080i e 1080p e mapear os pixels 1: 1 sem a necessidade de redefinir a amostra do conteúdo de vídeo para exibir na tela.",
+      },
+      {
+        title: "Operacional",
+        text: "Esse Monitor traz conexão Bivolt garantindo que você possa utilizar em qualquer lugar. Ele também traz tecnologia de conexão VGA e HDMI, suportando diferentes tipos de de compatibilidade para você.",
+      },
+      {
+        title: "Tecnologia de Tela",
+        text: "O monitor 22W-75KAN da Brazil PC conta com tecnologia de painel TN que garante rápido tempo de resposta, traz também um display que possui 16.7 Milhões de cores para maior fidelidade de cores para suas atividades do dia-a-dia como Netflix, trabalhos de faculdade e para seu Home Office.",
+      },
+      {
+        title: "Garanta já o seu Monitor Brazil PC de 21.5 LED no KaBuM!",
+        text: "",
+      },
+    ],
+  };
 
-function Produto () {
-    return (
-        <>
-
-        </>
-    )
+  return (
+    <>
+        <Main></Main>
+        <Description></Description>
+    </>
+  );
 }
 
-export default Produto
+const Main = styled.main` 
+
+`
+
+const Description = styled.div` 
+    
+`
+
+export default Produto;
